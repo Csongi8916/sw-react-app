ad1373d0e0177e50812b8a4c9d576449413c7670
Komár Csongor <csongi8916@gmail.com>
2024-03-18 19:31:24 +0100

    Pagination works!

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -3,25 +3,28 @@ import { fetchCharacters, fetchPlanet } from '../../http.js';
 import Card from '../Card/Card.jsx';
 import Spinner from '../Spinner/Spinner.jsx';
 import classes from './CardContainer.module.scss';
+import nextBtnImg from '../../assets/next-ico.png';
+import prevBtnImg from '../../assets/prev-ico.png';
 
 function CardContainer() {
   const [isFetching, setIsFetching] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
   const [characters, setCharacters] = useState([]);
   const [error, setError] = useState();
 
   let imageId = 0;
+  let pageIndex = 0;
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
 
       try {
-        const availableCharacters = await fetchCharacters();
+        const availableCharacters = await fetchCharacters(currentPage);
         const characters = await Promise.all(availableCharacters.map(async (character) => {
           const planet = await fetchPlanet(character.homeworld);
           return { ...character, planet };
         }));
-        console.log(characters);
         setCharacters(characters);
       } catch (error) {
         setError({
@@ -33,7 +36,19 @@ function CardContainer() {
       }
     }
     fetchPlaces();
-  }, []);
+  }, [currentPage]);
+
+  const handleNextCharacter = () => {
+    setCurrentPage(currentPage + 1);
+  }
+
+  const handlePrevCharacter = () => {
+    if (currentPage === 1) {
+      alert('There is not other previous pages!');
+      return;
+    }
+    setCurrentPage(currentPage - 1);
+  }
 
   if (error) {
     return <Error title="An error occurred!" message={error.message} />;
@@ -42,13 +57,26 @@ function CardContainer() {
   return (
     <>
       {isFetching ? (
-        <Spinner></Spinner>
+        <Spinner />
       ) : (
-      <section className={classes.flexContainer}>
-        {characters.map((character) => (
-          <Card character={character} imageId={++imageId} className={classes.swCard} />
-        ))}
-      </section>
+      <>
+        <section className={classes.flexContainer}>
+          {characters.map((character) => (
+            <Card character={character} imageId={++imageId} className={classes.swCard} />
+          ))}
+        </section>
+        <div style={{ display: 'flex', justifyContent: 'center'}}>
+          <h3>Actual Page Number: {currentPage}</h3>
+        </div>
+        <div className={classes.paginationArea}>
+          <button className={classes.paginastionBtn} onClick={handlePrevCharacter}>
+            <img src={prevBtnImg} alt="Next" />
+          </button>
+          <button className={classes.paginastionBtn} onClick={handleNextCharacter}>
+            <img src={nextBtnImg} alt="Next" />
+          </button>
+        </div>
+      </>
       )}
     </>
   );
--- src/http.js
+++ src/http.js
@@ -1,5 +1,5 @@
-export async function fetchCharacters() {
-  const response = await fetch('https://swapi.dev/api/people?page=1');
+export async function fetchCharacters(pageIndex) {
+  const response = await fetch(`https://swapi.dev/api/people?page=${pageIndex}`);
   const resData = await response.json();
   if (!response.ok) {
     throw new Error('Failed to fetch characters');
@@ -8,7 +8,7 @@ export async function fetchCharacters() {
 }
 
 export async function fetchPlanets() {
-  const response = await fetch('https://swapi.dev/api/planets/?page=1');
+  const response = await fetch('https://swapi.dev/api/planets/');
   const resData = await response.json();
   if (!response.ok) {
     throw new Error('Failed to fetch characters');
